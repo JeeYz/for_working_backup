6523470cc40a57bba4abf0e73315fb9af860ceb8
JeeYz <jkdspiegel@gmail.com>
2021-09-17 17:14:19 +0900

    backup

--- models/cw_data_processing.py
+++ models/cw_data_processing.py
@@ -4,18 +4,47 @@ from numpy.core.numeric import full
 from scipy.io import wavfile
 import numpy as np
 import json
+import librosa
 
 import time
+import copy
+import matplotlib.pyplot as plt
 
 
 FULL_SIZE = 20000
 
 
 def main():
-    prepro = Preprocessing_data()
-    result_data = prepro.preprocess_data()
-    print(len(result_data))
+    gen_data = control_gen_data()
+    gen_data.generate_data()
+
     return
+
+
+
+#%%
+class control_gen_data():
+    def __init__(self):
+        self.random_rate = 0.1
+
+    def generate_data(self):
+        prepro = Preprocessing_data()
+        result_data = prepro.preprocess_data()
+        print(len(result_data))
+
+        # for one in result_data:
+        #     print(type(one))
+
+        aug_data_pro = Augment_data()
+        return_data = aug_data_pro.read_data_list(result_data)
+        print(len(return_data))
+
+
+    def print_plt_data(self):
+        pass
+
+    def write_numpy_file(self):
+        pass
 
 
 
@@ -108,7 +137,14 @@ class Preprocessing_data():
 
             if tmp_len > tmp_range:
                 tmp = tmp_data[:self.full_size]
-                none_data_list.append(tmp)
+                mid_point = -1
+
+                temp_dict = dict()
+                temp_dict['data'] = tmp
+                temp_dict['label'] = 'none'
+                temp_dict['mid_val'] = mid_point
+
+                none_data_list.append(temp_dict)
 
                 tmp_num = -1*self.full_size
                 tmp = tmp_data[tmp_num:]
@@ -121,7 +157,7 @@ class Preprocessing_data():
                 mid_point = -1
 
             else:
-                min_point = self.full_size//2
+                mid_point = self.full_size//2
                 tmp = self.add_head_and_tail_none(tmp_data)
             
             temp_dict = dict()
@@ -204,6 +240,7 @@ class Preprocessing_data():
         label_name = filename.split("\\")[-2]
 
         temp_dict = dict()
+
         temp_dict["data"] = data
         temp_dict['label'] = label_name
         temp_dict['mid_val'] = mid_point
@@ -356,28 +393,110 @@ class Preprocessing_data():
 
 #%%
 class Augment_data():
-    def __init__(self, input_data_list):
+    def __init__(self):
 
+        self.full_size = FULL_SIZE
         self.augmented_data = list()
         self.time_stretch_list = [
-                0.9,
+                # 0.9,
                 1.0,
                 1.1,
                 ]
-        self.position_aug_frame = 100
+        self.position_aug_frame = 1000
 
-        # 데이터를 잘라내고
+        self.threshold_rate = 0.15
+        self.noise_threshold_rate = 0.05
 
-        # 위치를 랜덤으로 배치하고
+        self.para_num = 0
 
-        # 타임 스트레치를 적용하고
+
+    def add_noise_data(self, input_data, asize):
+
+        low_val = np.min(input_data)*self.noise_threshold_rate
+        high_val = np.max(input_data)*self.noise_threshold_rate
+
+        front_random_data = np.random.randint(low_val, high_val, asize)
+
+        return front_random_data
+
+
+    def gen_aug_data(self, data_dict, one_data):
+
+        new_data_list = list()
+
+        init_start_idx = data_dict['start'][0]
+        init_data = copy.deepcopy(data_dict['data'])
+        init_data = np.array(init_data, dtype=np.float64)
+        init_label = one_data['label']
+
+        for_cond = len(data_dict['start'])
+
+        for r in self.time_stretch_list:
+
+            aug_data = librosa.effects.time_stretch(init_data, r)
+
+            for one_st in range(for_cond):
+                temp = dict()
+
+                cond1 = one_st - init_start_idx
+                add_size = copy.deepcopy(cond1)
+
+                if add_size != 0:
+                    add_size = np.abs(add_size)
+                    return_noise = self.add_noise_data(init_data, add_size)
+                    return_noise = np.array(return_noise, dtype=np.float64)
+                    if cond1 > 0:
+                        temp_data = np.append(return_noise, aug_data)
+                    else:
+                        temp_data = np.append(aug_data, return_noise)
+
+                    new_data = temp_data[0: self.full_size]
+                else:
+                    new_data = copy.deepcopy(init_data)
+                
+                temp['data'] = new_data
+                temp['label'] = init_label
+
+                new_data_list.append(temp)
+                self.para_num += 1
+                print("{} th data generated...".format(self.para_num), end='\r')
+
+        return new_data_list
 
 
     def make_aug_list(self, data_dict):
 
-        
+        start_list = list()
+        end_list = list()
 
-        return
+        tmp_start = data_dict['start']
+        tmp_end = data_dict['end']
+
+        start_list.append(tmp_start)
+        end_list.append(tmp_end)
+
+        gap_start_end = tmp_end - tmp_start
+
+        temp = copy.deepcopy(tmp_start)
+        while True:
+            temp  = temp - self.position_aug_frame
+            if temp < 0:
+                break
+            start_list.append(temp)
+            end_list.append(temp+gap_start_end)
+
+        temp = copy.deepcopy(tmp_start)
+        while True:
+            temp  = temp + self.position_aug_frame
+            if (temp+gap_start_end) > self.full_size:
+                break
+            start_list.append(temp)
+            end_list.append(temp+gap_start_end)
+
+        data_dict['start'] = start_list
+        data_dict['end'] = end_list
+
+        return data_dict
 
 
     def new_start_end_index(self, data_dict, temp_class):
@@ -391,29 +510,38 @@ class Augment_data():
         if tmp_start < 0:
             data_dict['start'] = 0
 
-        if tmp_end > temp_class.full_size:
-            end_idx['end'] = temp_class.full_size
+        if tmp_end > self.full_size:
+            data_dict['end'] = self.full_size
 
         return data_dict
 
 
     def read_data_list(self, input_list):
 
-        new_dict = dict()
+        new_list = list()
         temp_class = Preprocessing_data()
 
         for one_data in input_list:
             # data, label, mid_val
-            tmp_data = one_data['data']
-            tmp_label = one_data['label']
-            tmp_mid = one_data['mid_val']
+            # print(type(one_data))
+            try:
+                tmp_data = one_data['data']
+            except IndexError:
+                print(one_data)
+                print(len(one_data))
+                continue
             
-            # return data, start, end
+            # return data = data, start = list, end = list
             return_dict = temp_class.cut_one_wav_file(tmp_data)
             return_dict = self.new_start_end_index(return_dict, temp_class)
-            self.make_aug_list(return_dict)
+            return_dict = self.make_aug_list(return_dict)
+            gen_aug_data = self.gen_aug_data(return_dict, one_data)
+            new_list.extend(gen_aug_data)
 
-        return
+        return new_list
+
+
+
 
 
 
