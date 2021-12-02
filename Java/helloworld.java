bd814542699e0a02dfa1b40c0a810038b705b080
JeeYz <jkdspiegel@gmail.com>
2021-12-02 10:40:46 +0900

    back up

--- Java/helloworld.java
+++ Java/helloworld.java
@@ -1,9 +1,5 @@
 public class helloworld {
-    public static void main(String[] args){
-        System.out.println("Hello World~!!");
-        System.out.println("What the Hell Are You Waiting For~!!");
+    public static void main(String[] arg){
+        System.out.println("hello, world~!!");
     }
 }
-
-
-
